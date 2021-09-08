9b36f510292c9eebc7ede4bc231b06cd6a7e09d4
Pedro Henrique <pedroh.ba04@gmail.com>
2021-09-08 14:15:00 -0300

    Usando .env para configurações

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const connection = new Sequelize('guiapress', 'root', '',{
-    host: 'localhost',
+const { NAME_DATABASE, DATABASE_USER, PASSWORD, HOST } = process.env;
+
+console.log(NAME_DATABASE, DATABASE_USER, PASSWORD, HOST);
+
+const connection = new Sequelize(NAME_DATABASE, DATABASE_USER, PASSWORD,{
+    host: HOST,
     dialect : 'mysql',
     timezone : '-03:00'
 });
